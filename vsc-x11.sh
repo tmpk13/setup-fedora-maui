e44b09f982f98b42045714794b386bdce099a36d
tmpk13 <167717442+tmpk13@users.noreply.github.com>
2025-11-23 04:05:26 -0800

    Script to run vsc on vm x11 forward

---
+++ vsc-x11.sh
@@ -0,0 +1,12 @@
+qemu-system-x86_64 \
+  -drive file=ubu-maui.qcow2,format=qcow2 \
+  -m 22G \
+  -cpu host \
+  -enable-kvm \
+  -smp cores=4,threads=2,sockets=1 \
+  -machine vmport=off \
+  -display none \
+  -netdev user,id=net0,hostfwd=tcp::2222-:22 \
+  -device virtio-net-pci,netdev=net0
+# ssh -XC -p 2222 user@localhost # -t "code"
+# code
